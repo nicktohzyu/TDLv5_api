1cd37094eee929914e3ff71d89f9ab8a02b6a69f
nicktohzyu <34980083+nicktohzyu@users.noreply.github.com>
2020-01-08 22:37:42 +0800

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "*"
+      origins "http://localhost:3000"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+  
+    allow do
+      origins "https://gifted-swartz-475d8e.netlify.com/"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
   end
