e48ab35657f1613264c196cb0e95a651220cfac9
nicktohzyu <34980083+nicktohzyu@users.noreply.github.com>
2020-01-10 00:42:42 +0800

    front end cord address

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
   
     allow do
+      origins "https://gifted-swartz-475d8e.netlify.com"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+    
+    allow do
       origins "https://nicktohzyu-tdlv5-front.netlify.com/"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
