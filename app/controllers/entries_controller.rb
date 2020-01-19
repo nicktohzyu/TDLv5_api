03c610cb4fb7cca14a12e679dce5081233e9ccaa
nicktohzyu <nicholastohzhenyu@gmail.com>
2020-01-19 15:53:55 +0800

    Update entries_controller.rb

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,35 +1,56 @@
 class EntriesController < ApplicationController
-    include CurrentUserConcern
-    before_action :set_entry, only: [:show, :edit, :update, :destroy]
-    def index
-        @entries = Entry.where(user_id: @current_user.id)
-        # if(params[:tag])
-        #   @entries = @entries.tagged_with(params[:tag])
-        # end
-        render json: @entries
+  include CurrentUserConcern
+  before_action :set_entry, only: [:show, :update, :destroy]
+  def index
+    @entries = Entry.where(user_id: @current_user.id)
+    # if(params[:tag])
+    #   @entries = @entries.tagged_with(params[:tag])
+    # end
+    render json: @entries
+  end
+  
+  def show
+    render json: @entry
+  end
+  
+  def create
+    @entry = @current_user.entries.new(entry_params)
+    if @entry.save
+      render json: @entry
+    else 
+      render json: { error: @entry.errors.full_messages }, status: 400
     end
+  end
 
-    def show
-        render json: @entry
-    end
-
-    def create
-        @entry = @current_user.entries.new(entry_params)
-        if @entry.save
-            render json: @entry
-        else 
-            render json: { error: @entry.errors.full_messages }, status: 400
-        end
+  def update
+    if @entry
+      if @entry.update(entry_params)
+        render json: { message: 'Entry updated' }, status: 200
+      else 
+        render json: { error: @entry.errors.full_messages }, status: 400
       end
-
-    private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_entry
-        @entry = Entry.find(params[:id])
+    else
+      render json: { error: 'Entry not found' }, status: 404 #correct http code?
     end
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def entry_params
-        params.require(:entry).permit(:content, :due_date, :user_id, :tag_list, :tag, { tag_ids: [] }, :tag_ids)
+  def destroy
+    if @entry
+      if @entry.destroy
+        render json: { message: 'Entry deleted.' }, status: 200
+      else
+        render json: { error: @entry.errors.full_messages }, status: 400
+      end
+    else
+      render json: { error: 'Entry not found' }, status: 404 #correct http code?
     end
+  end
+  private
+  def set_entry
+    @entry = Entry.find(params[:id])
+  end
+  
+  def entry_params
+    params.require(:entry).permit(:content, :due_date, :done, :user_id, :tag_list, :tag, { tag_ids: [] }, :tag_ids)
+  end
 end
