af5e216f1fb49aeabcaef8ebab0aa5b05a5b64a3
nicktohzyu <nicholastohzhenyu@gmail.com>
2020-01-22 12:04:58 +0800

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,4 +8,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       origins "https://nicktohzyu-tdlv5-front.netlify.com"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
+
+    allow do
+      origins "https://nicktohzyu-tdlv5-front.herokuapp.com"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+    
   end
