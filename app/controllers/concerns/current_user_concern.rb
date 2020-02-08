70fc6c620df2d9d8c2ac8768e94e6a302bbe308c
nicktohzyu <34980083+nicktohzyu@users.noreply.github.com>
2020-02-08 17:19:47 +0800

    hardcode user hack

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,10 +6,11 @@ module CurrentUserConcern
   end
 
   def set_current_user
-    puts "in currentUserConcern set_current_user"
-    puts session[:user_id]
-    if session[:user_id]
-      @current_user = User.find(session[:user_id])
-    end
+    @current_user = User.first
+    # puts "in currentUserConcern set_current_user"
+    # puts session[:user_id]
+    # if session[:user_id]
+    #   @current_user = User.find(session[:user_id])
+    # end
   end
 end
